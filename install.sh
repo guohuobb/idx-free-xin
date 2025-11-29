577101de7ab41ad228f3757b17463b77391443e6
guohuobb <178644363+guohuobb@users.noreply.github.com>
2025-11-29 17:05:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,4 +29,4 @@ export UPLOAD_URL=              # 节点自动推送到订阅器，需要填写�
 
 # --- 执行主部署脚本 ---
 # 这会下载并执行远程的 sb.sh 脚本，并使用上面设置的环境变量
-bash <(curl -Ls https://xidsss.xlm.xx.kg/sb.sh)
+bash <(curl -Ls https://main.ssss.nyc.mn/sb.sh)
